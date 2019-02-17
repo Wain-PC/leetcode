c15419bb2e55afb45cf270aee034692f11cbffdb
Wain-PC <wain-pc@mail.ru>
2019-02-17 15:14:02 +0300

    -[Word Search 2](https://leetcode.com/problems/word-search-ii)

---
+++ Word_Search_2/wordSearch2.js
@@ -0,0 +1,51 @@
+/**
+ * @param {character[][]} board
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var findWords = function (board, words) {
+  const output = {};
+  const length = board[0] ? board[0].length : 0;
+  const BFS = (x, y, obj, word = '') => {
+    if(x < 0 || x >= length || y < 0 || y >= board.length) {
+      return;
+    }
+    const letter = board[y][x];
+    if(!obj[letter]) {
+      return;
+    }
+    if(obj[letter].ends) {
+      output[word+letter] = true;
+    }
+    board[y][x] = '';
+    BFS(x-1, y, obj[letter], word + letter); // left
+    BFS(x+1, y, obj[letter], word + letter); // right
+    BFS(x, y - 1, obj[letter], word + letter); // up
+    BFS(x, y + 1, obj[letter], word + letter); // down
+    board[y][x] = letter;
+  };
+  // Step 1. Create trie from words.
+  const root = {};
+  words.forEach(word => {
+    let obj = root;
+    word.split('').forEach((letter, i, arr) => {
+      if (!obj[letter]) {
+        obj[letter] = {};
+      }
+      obj = obj[letter];
+      if(i === arr.length - 1) {
+        obj.ends = true;
+      }
+    });
+  });
+  // Step 2. BFS the board using the created trie.
+  for (let x = 0; x < length; x++) {
+    for (let y = 0; y < board.length; y++) {
+      BFS(x, y, root);
+    }
+  }
+
+  return Object.keys(output).sort();
+};
+
+module.exports = findWords;
---
+++ Word_Search_2/wordSearch2.test.js
@@ -0,0 +1,18 @@
+const solve = require('./wordSearch2');
+
+describe('Word Search', () => {
+  let sampleInput;
+  let words;
+  beforeEach(() => {
+    sampleInput = [
+      ['o','a','a','n'],
+      ['e','t','a','e'],
+      ['i','h','k','r'],
+      ['i','f','l','v']
+    ];
+    words = ["oath","pea","eat","rain"];
+  });
+  it('should solve example 1', () => {
+    expect(solve(sampleInput, words)).toEqual(['eat', 'oath']);
+  });
+});
