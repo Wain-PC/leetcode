720977a1f81aa265357b474760ab6cd3361ad444
Wain-PC <wain-pc@mail.ru>
2019-02-17 15:14:39 +0300

    -Minor style fix

--- Word_Search_2/wordSearch2.js
+++ Word_Search_2/wordSearch2.js
@@ -7,19 +7,19 @@ var findWords = function (board, words) {
   const output = {};
   const length = board[0] ? board[0].length : 0;
   const BFS = (x, y, obj, word = '') => {
-    if(x < 0 || x >= length || y < 0 || y >= board.length) {
+    if (x < 0 || x >= length || y < 0 || y >= board.length) {
       return;
     }
     const letter = board[y][x];
-    if(!obj[letter]) {
+    if (!obj[letter]) {
       return;
     }
-    if(obj[letter].ends) {
-      output[word+letter] = true;
+    if (obj[letter].ends) {
+      output[word + letter] = true;
     }
     board[y][x] = '';
-    BFS(x-1, y, obj[letter], word + letter); // left
-    BFS(x+1, y, obj[letter], word + letter); // right
+    BFS(x - 1, y, obj[letter], word + letter); // left
+    BFS(x + 1, y, obj[letter], word + letter); // right
     BFS(x, y - 1, obj[letter], word + letter); // up
     BFS(x, y + 1, obj[letter], word + letter); // down
     board[y][x] = letter;
@@ -33,7 +33,7 @@ var findWords = function (board, words) {
         obj[letter] = {};
       }
       obj = obj[letter];
-      if(i === arr.length - 1) {
+      if (i === arr.length - 1) {
         obj.ends = true;
       }
     });
